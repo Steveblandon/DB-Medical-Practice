629f4f95a53a5905367b865d6a9599b296baf26e
Steve Blandon <steveblandon@gmail.com>
2016-03-25 20:55:08 -0400

    DB connect from index

--- www/dbproj/index.php
+++ www/dbproj/index.php
@@ -1,7 +1,17 @@
 <html>
 <body>
 <?php
-echo "Test 1 2 3";
+$serverName = "localhost";
+$username = "root";
+$password = "";
+$database = "dbproj";
+
+$conn = new mysqli($serverName, $username, $password, $database);
+
+if ($conn->connect_error){
+	die("Connection failed" . $conn->connect_error);
+}
+else echo "Connected to database successfully <br>";
 ?>
 </body>
 </html>
