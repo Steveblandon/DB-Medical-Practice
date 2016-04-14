27a3249614413da320cd3802797543e2231e010a
Seana36 <sean.emily11@gmail.com>
2016-04-13 21:50:42 -0400

    doctor php changes

--- www/dbproj/doctor.php
+++ www/dbproj/doctor.php
@@ -18,11 +18,69 @@ else{
 	echo "";
 } 
 
-/////////////////view patient info
+if ( isset( $_POST['Submit1'] ) ) { 
+	/////////////////view patient info
+		echo "Patient Info: ";
+		$sql = "SELECT * FROM patient";
+		$result = $conn -> query($sql);
+		$count = 1; 
+		echo "<table border = '1'>";
+		if($result -> num_rows > 0 ){
+			while($row = $result->fetch_assoc()) {
+				if($count == 1){
+					echo "<tr>";
+				   	echo "<td>Patient ID: </td>".
+				   		" <td>Name: </td>".
+				   		" <td>Sex: </td>".
+				   		" <td>Date of Birth </td>".
+				   		" <td>Address </td>".
+				   		" <td>Phone Number: </td>".
+				   		" <td>Insurance: </td>".
+				   		" <td>Insurance Number: </td>".
+				   		" <td>Insurance Primary: </td>"; 
+				   	echo "</tr>";
+				    echo "<tr>";
+				   	echo "<td>". $row["patientID"]."</td>".
+				   		" <td>". $row["fName"]." " .$row["lName"]."</td>".
+				   		" <td>". $row["sex"]. "</td>".
+				   		" <td>". $row["DOB"]. "</td>".
+				   		" <td>". $row["address"]. "</td>".
+				   		" <td>". $row["phoneNo"]. "</td>".
+				   		" <td>". $row["insuranceProvider"]. "</td>".
+				   		" <td>". $row["insuranceNo"]. "</td>".
+				   		" <td>". $row["insurancePrimary"]. "</td>"; 
+				   	echo "</tr>";
+				   	$count++;
+			 	  }
+			 	  else {
+			 	  	echo "<tr>";
+				   	echo "<td>". $row["patientID"]."</td>".
+				   		" <td>". $row["fName"]." " .$row["lName"]."</td>".
+				   		" <td>". $row["sex"]. "</td>".
+				   		" <td>". $row["DOB"]. "</td>".
+				   		" <td>". $row["address"]. "</td>".
+				   		" <td>". $row["phoneNo"]. "</td>".
+				   		" <td>". $row["insuranceProvider"]. "</td>".
+				   		" <td>". $row["insuranceNo"]. "</td>".
+				   		" <td>". $row["insurancePrimary"]. "</td>"; 
+				   	echo "</tr>";
+			 	  }
+			   }
+			}
+		echo "</table>";
+		}
+else if( isset( $_POST['Submit2'] ) ) { 
+	/////////////////edit patient info
 
-/////////////////edit patient info
- 
-/////////////////see thier schedule for the day 
+		echo "yo";
+		}
+else if( isset( $_POST['Submit3'] ) ) {
+		/////////////////see thier schedule for the day 
+		}
+else {
+		echo "GOODBYE";
+		}
+
 		   	
 ?> 
 
